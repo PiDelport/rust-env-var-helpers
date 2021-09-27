8aed1f58daf2e68b880e2dc0defa3650987e69c6
Pi Delport <pjdelport@gmail.com>
2021-09-27 23:23:10 +0200

    feat: use "doc-include-str" feature flag for lib doc (#2)
    This allows support for older Rust: MSRV 1.54 → 1.46

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
-#![doc = include_str!("../README.md")]
+// Conditionally use include_str, for Rust < 1.54.
+// See: https://github.com/rust-lang/rust/issues/82768#issuecomment-803935643
+#![cfg_attr(feature = "doc", feature(extended_key_value_attributes))]
+#![cfg_attr(feature = "doc", cfg_attr(feature = "doc", doc = include_str!("../README.md")))]
 
 pub mod env_vars;
